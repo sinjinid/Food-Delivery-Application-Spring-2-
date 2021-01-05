2121ffa410d6bc9047a3c1cc48eae07aaa486055
Nishachat <76964162+Nishachat@users.noreply.github.com>
2021-01-05 09:41:40 +0530

    Create CustomerRestaurantException.java

---
+++ exception/CustomerRestaurantException.java
@@ -0,0 +1,9 @@
+package com.sprint.exception;
+
+public class CustomerRestaurantException extends Exception{
+	private static final long serialVersionUID = 1L;
+
+	public CustomerRestaurantException(String message)	{
+		super(message);
+	}
+}
